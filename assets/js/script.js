7521207b7a88740e0300a0c16d842bfd74cb0d2c
D'Angelo <dangelokwakye@gmail.com>
2024-01-19 09:59:59 +0000

    header section completed

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/* 
+* adding event on elements
+*/
+
+const addEventOnELement = function(elem, type, callback){
+    if(elem.length > 1){
+        for(let i = 0; i< elem.length; i++){
+            elem[i].addEventListener(type, callback)
+        }
+    } else{
+        elem.addEventListener(type, callback)
+    }
+}
+
+
+/* 
+* navbar toggle
+ */ 
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggle = document.querySelectorAll("[data-nav-toggler]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function (){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+
+addEventOnELement(navToggle, "click", toggleNavbar);
+
+
+const closeNavbar = function (){
+    navbar.classList.remove("active")
+    overlay.classList.remove("active")
+}
+
+addEventOnELement(navbarLinks, "click", closeNavbar);
+
+const header = document.querySelector("[data-header]")
+
+const headerActive = function() {
+    if (window.scrollY > 100) {
+        header.classList.add("active")
+    }else{
+        header.classList.remove("active")
+    };
+};
+
+addEventOnELement(window, "scroll", headerActive);
